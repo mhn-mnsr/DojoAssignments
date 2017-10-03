de82d29dc25a5c3301113cae9043a6539e90c67d
Maheen Mansoor <mhn1211@gmail.com>
2017-10-03 18:44:09 -0500

    Making Dictionaries from lists

---
+++ Python/lists_to_dict.py
@@ -0,0 +1,8 @@
+def make_dict(arr1, arr2):
+
+    my_dict = dict(zip(name, favorite_animal))
+    print(my_dict)
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+make_dict(name,favorite_animal)
