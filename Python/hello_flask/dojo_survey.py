7451e4366080d8554f00d346b9cdf69d5e5e4445
Maheen Mansoor <mhn1211@gmail.com>
2017-10-09 20:27:35 -0500

    Final draft for dojo survey- validation

--- Python/hello_flask/dojo_survey.py
+++ Python/hello_flask/dojo_survey.py
@@ -12,35 +12,17 @@ def create_user():
     name = request.form['name']
     if len(request.form['name']) < 1:
         flash("Name cannot be empty!")
-    elif len(request.form['name']) > 1:
-        flash("Success! Your name is {}".format(request.form['name']))
+        return redirect('/')
     else:
-        pass
-    return redirect('/')
-
-@app.route('/process', methods=['POST'])
-def create_users():
+        flash("Success! Your name is {}".format(request.form['name']))
     DojoLocation = request.form['location']
     FavoriteLanguage = request.form['language']
     textarea = request.form['comment']
-    return render_template('survey_result.html', location = DojoLocation, language = FavoriteLanguage , comment = textarea)
+    if len(request.form['comment']) > 120:
+        flash("Comments too long!")
+        return redirect('/')
+    return render_template('survey_result.html', name = name, location = DojoLocation, language = FavoriteLanguage , comment = textarea)
 
-# @app.route('/process', methods=['POST'])
-# def name_length():
-#     # print len(request.form['name'])
-#     if len(request.form['name']) < 1:
-#         flash("Name cannot be empty!")
-#     else:
-#         flash("Success! Your name is {}".format(request.form['name']))
-#     return redirect('/')
-
-# @app.route('/process', methods=['POST'])
-# def comment_length():
-#     if len(request.form['comment']) > 120:
-#         flash("Your message is more than 120 characters")
-#     else:
-#         flash("Success, printing your message!")
-#     return redirect('/')
 # @app.route('/form_result')
 # def form_display():
 #     print request.form
